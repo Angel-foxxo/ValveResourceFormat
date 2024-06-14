ea614d526642c06331835a1f63c388d7a303c90d
Angel <angelcazacu8@gmail.com>
2024-06-15 00:07:52 +0200

    Revert "still pretty fed up implementation"
    This reverts commit e01b1d49a069a55a2fdbadb707f13c309690f6d4.

--- GUI/MainForm.Frame.cs
+++ GUI/MainForm.Frame.cs
@@ -1,5 +1,4 @@
 using System.Drawing;
-using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using GUI.Utils;
@@ -42,12 +41,12 @@ partial class MainForm
     protected override void OnActivated(EventArgs e)
     {
         base.OnActivated(e);
-    
+
         var margins = new Windows.Win32.UI.Controls.MARGINS
         {
-            cyTopHeight = 24,// TODO menuStrip.Size.Height is 0 on startup
+            cyTopHeight = 24, // TODO menuStrip.Size.Height is 0 on startup
         };
-    
+
         _ = PInvoke.DwmExtendFrameIntoClientArea((Windows.Win32.Foundation.HWND)Handle, margins);
     }
 
@@ -67,170 +66,60 @@ partial class MainForm
     }
     */
 
-    private bool win32_window_is_maximized(Windows.Win32.Foundation.HWND handle)
-    {
-        Windows.Win32.UI.WindowsAndMessaging.WINDOWPLACEMENT placement = new();
-        unsafe
-        {
-            placement.length = (uint)sizeof(Windows.Win32.UI.WindowsAndMessaging.WINDOWPLACEMENT);
-
-            if (Windows.Win32.PInvoke.GetWindowPlacement(handle, &placement))
-            {
-                return placement.showCmd == Windows.Win32.UI.WindowsAndMessaging.SHOW_WINDOW_CMD.SW_SHOWMAXIMIZED;
-            }
-        }
-        return false;
-    }
-
-    static int win32_dpi_scale(int value, uint dpi)
-    {
-        return (int)((float)value * dpi / 96);
-    }
-
-    static Windows.Win32.Foundation.RECT win32_titlebar_rect(Windows.Win32.Foundation.HWND handle)
-    {
-        Windows.Win32.Foundation.SIZE title_bar_size = new();
-        title_bar_size.cy = 0;
-        title_bar_size.cx = 0;
-
-        var WP_CAPTION = 1;
-        var CS_ACTIVE = 1;
-
-
-        const int top_and_bottom_borders = 2;
-        Windows.Win32.CloseThemeDataSafeHandle theme = Windows.Win32.PInvoke.OpenThemeData(handle, "WINDOW");
-        uint dpi = Windows.Win32.PInvoke.GetDpiForWindow(handle);
-        Windows.Win32.PInvoke.GetThemePartSize(theme, new Windows.Win32.Graphics.Gdi.HDC(), WP_CAPTION, CS_ACTIVE, null,
-            Windows.Win32.UI.Controls.THEMESIZE.TS_TRUE,
-            out title_bar_size);
-        Windows.Win32.PInvoke.CloseThemeData((Windows.Win32.UI.Controls.HTHEME)theme.DangerousGetHandle());
-
-        int height = win32_dpi_scale(title_bar_size.cy, dpi) + top_and_bottom_borders;
-
-        Windows.Win32.Foundation.RECT rect;
-        Windows.Win32.PInvoke.GetClientRect(handle, out rect);
-        rect.bottom = rect.top + height;
-        return rect;
-    }
-
-    static Windows.Win32.Foundation.LRESULT HTNOWHERE = (Windows.Win32.Foundation.LRESULT)0;
-    static Windows.Win32.Foundation.LRESULT HTRIGHT = (Windows.Win32.Foundation.LRESULT)11;
-    static Windows.Win32.Foundation.LRESULT HTLEFT = (Windows.Win32.Foundation.LRESULT)10;
-    static Windows.Win32.Foundation.LRESULT HTTOPLEFT = (Windows.Win32.Foundation.LRESULT)13;
-    static Windows.Win32.Foundation.LRESULT HTTOP = (Windows.Win32.Foundation.LRESULT)12;
-    static Windows.Win32.Foundation.LRESULT HTTOPRIGHT = (Windows.Win32.Foundation.LRESULT)14;
-    static Windows.Win32.Foundation.LRESULT HTBOTTOMRIGHT = (Windows.Win32.Foundation.LRESULT)17;
-    static Windows.Win32.Foundation.LRESULT HTBOTTOM = (Windows.Win32.Foundation.LRESULT)15;
-    static Windows.Win32.Foundation.LRESULT HTBOTTOMLEFT = (Windows.Win32.Foundation.LRESULT)16;
-
     protected override void WndProc(ref Message m)
     {
-        if (m.Msg == PInvoke.WM_NCCALCSIZE)
+        if (m.Msg == PInvoke.WM_NCCALCSIZE && (int)m.WParam == 1)
         {
-            if (m.WParam == 0)
-            {
-                m.Result = Windows.Win32.PInvoke.DefWindowProc(
-                    (Windows.Win32.Foundation.HWND)m.HWnd,
-                    (uint)m.Msg,
-                    (Windows.Win32.Foundation.WPARAM)(nuint)m.WParam,
-                    (Windows.Win32.Foundation.LPARAM)m.LParam);
+            var frameX = PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CXFRAME, (uint)DeviceDpi);
+            var frameY = PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CYFRAME, (uint)DeviceDpi);
+            var padding = PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CXPADDEDBORDER, (uint)DeviceDpi);
 
-                return;
-            }
-
-            var dpi = Windows.Win32.PInvoke.GetDpiForWindow((Windows.Win32.Foundation.HWND)this.Handle);
-            var frameX = Windows.Win32.PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CXFRAME, dpi);
-            var frameY = Windows.Win32.PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CYFRAME, dpi);
-            var padding = Windows.Win32.PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CXPADDEDBORDER, dpi);
-
-            Windows.Win32.UI.WindowsAndMessaging.NCCALCSIZE_PARAMS nccsp = (Windows.Win32.UI.WindowsAndMessaging.NCCALCSIZE_PARAMS)Marshal.PtrToStructure(m.LParam, typeof(Windows.Win32.UI.WindowsAndMessaging.NCCALCSIZE_PARAMS));
-
+            var nccsp = Marshal.PtrToStructure<Windows.Win32.UI.WindowsAndMessaging.NCCALCSIZE_PARAMS>(m.LParam);
+            nccsp.rgrc._0.bottom -= frameY + padding;
             nccsp.rgrc._0.right -= frameX + padding;
             nccsp.rgrc._0.left += frameX + padding;
-            nccsp.rgrc._0.bottom -= frameY + padding;
 
-            if (win32_window_is_maximized((Windows.Win32.Foundation.HWND)this.Handle))
+            Windows.Win32.UI.WindowsAndMessaging.WINDOWPLACEMENT placement = default;
+            PInvoke.GetWindowPlacement((Windows.Win32.Foundation.HWND)Handle, ref placement);
+
+            if (placement.showCmd == Windows.Win32.UI.WindowsAndMessaging.SHOW_WINDOW_CMD.SW_SHOWMAXIMIZED)
             {
                 nccsp.rgrc._0.top += padding;
-            }
-            else
-            {
-                nccsp.rgrc._0.top += -1;
             }
 
             Marshal.StructureToPtr(nccsp, m.LParam, false);
 
-            m.Result = 0;
+            m.Result = IntPtr.Zero;
         }
-        //else if (m.Msg == PInvoke.WM_CREATE)
-        //{
-        //    Windows.Win32.UI.WindowsAndMessaging.SET_WINDOW_POS_FLAGS windowFlags =
-        //        Windows.Win32.UI.WindowsAndMessaging.SET_WINDOW_POS_FLAGS.SWP_FRAMECHANGED |
-        //        Windows.Win32.UI.WindowsAndMessaging.SET_WINDOW_POS_FLAGS.SWP_NOMOVE |
-        //        Windows.Win32.UI.WindowsAndMessaging.SET_WINDOW_POS_FLAGS.SWP_NOSIZE;
-        //
-        //    Windows.Win32.Foundation.RECT windowRect;
-        //    Windows.Win32.PInvoke.GetWindowRect((Windows.Win32.Foundation.HWND)this.Handle, out windowRect);
-        //    Windows.Win32.PInvoke.SetWindowPos((Windows.Win32.Foundation.HWND)this.Handle,
-        //        (Windows.Win32.Foundation.HWND)this.Handle,
-        //        windowRect.left,
-        //        windowRect.top,
-        //        windowRect.right - windowRect.left,
-        //        windowRect.bottom - windowRect.top,
-        //        windowFlags
-        //        );
-        //
-        //    return;
-        //}
-        //else if (m.Msg == PInvoke.WM_ACTIVATE)
-        //{
-        //    Windows.Win32.Foundation.RECT title_bar_rect = win32_titlebar_rect((Windows.Win32.Foundation.HWND)this.Handle);
-        //    Windows.Win32.PInvoke.InvalidateRect((Windows.Win32.Foundation.HWND)this.Handle, title_bar_rect, false);
-        //
-        //    m.Result = Windows.Win32.PInvoke.DefWindowProc(
-        //            (Windows.Win32.Foundation.HWND)m.HWnd,
-        //            (uint)m.Msg,
-        //            (Windows.Win32.Foundation.WPARAM)(nuint)m.WParam,
-        //            (Windows.Win32.Foundation.LPARAM)m.LParam);
-        //
-        //    return;
-        //}
         else if (m.Msg == PInvoke.WM_NCHITTEST)
         {
-            Windows.Win32.Foundation.LRESULT hit;
-            hit = Windows.Win32.PInvoke.DefWindowProc(
-                    (Windows.Win32.Foundation.HWND)m.HWnd,
-                    (uint)m.Msg,
-                    (Windows.Win32.Foundation.WPARAM)(nuint)m.WParam,
-                    (Windows.Win32.Foundation.LPARAM)m.LParam);
+            var dwmHandled = PInvoke.DwmDefWindowProc((Windows.Win32.Foundation.HWND)m.HWnd, (uint)m.Msg, new Windows.Win32.Foundation.WPARAM((nuint)m.WParam), new Windows.Win32.Foundation.LPARAM(m.LParam), out var result);
 
-            if (hit == HTNOWHERE || hit == HTRIGHT || hit == HTLEFT || hit == HTTOPLEFT || hit == HTTOP ||
-                hit == HTTOPRIGHT || hit == HTBOTTOMRIGHT || hit == HTBOTTOM || hit == HTBOTTOMLEFT)
+            if (dwmHandled == 1)
             {
-                m.Result = hit;
+                m.Result = result;
                 return;
             }
 
-            uint dpi = PInvoke.GetDpiForWindow((Windows.Win32.Foundation.HWND)Handle);
-            int frame_y = PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CYFRAME, dpi);
-            int padding = PInvoke.GetSystemMetricsForDpi(Windows.Win32.UI.WindowsAndMessaging.SYSTEM_METRICS_INDEX.SM_CXPADDEDBORDER, dpi);
-
+            // Convert to client coordinates
+            // TODO: easier word conversion?
             var point = PointToClient(new Point(LoWord((int)m.LParam), HiWord((int)m.LParam)));
 
-            if (point.Y > 0 && point.Y < frame_y + padding)
+            // TODO: This is not triggered when hovering over the menu strip except on very left - can't drag the window
+            // TODO: Buttons dont work when maximized
+            if (point.Y < menuStrip.Height)
             {
-                m.Result = 12;
+                m.Result = new IntPtr(PInvoke.HTCAPTION);
                 return;
             }
 
-            m.Result = 1;
-            return;
+            //m.Result = HitTestNCA(m.HWnd, m.WParam, m.LParam);
+            base.WndProc(ref m);
         }
         else
         {
             base.WndProc(ref m);
         }
-
     }
 
 #if false
