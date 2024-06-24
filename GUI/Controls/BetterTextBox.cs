70c2ce3f24cfb45345425850a04c63117cbe71a6
Angel <angelcazacu8@gmail.com>
2024-06-24 16:20:38 +0200

    fix slow resizing

--- GUI/Controls/BetterTextBox.cs
+++ GUI/Controls/BetterTextBox.cs
@@ -1,5 +1,6 @@
 using System.Drawing;
 using System.Windows.Forms;
+using Windows.Win32;
 
 namespace GUI.Controls
 {
@@ -16,12 +17,19 @@ namespace GUI.Controls
 
         protected override void WndProc(ref Message m)
         {
-            base.WndProc(ref m);
+            if (m.Msg == PInvoke.WM_PAINT)
+            {
+                base.WndProc(ref m);
 
-            var dc = Windows.Win32.PInvoke.GetWindowDC((Windows.Win32.Foundation.HWND)Handle);
-            using var g = Graphics.FromHdc(dc);
-            using var borderPen = new Pen(BorderColor);
-            g.DrawRectangle(borderPen, 0, 0, Width - 1, Height - 1);
+                var dc = Windows.Win32.PInvoke.GetWindowDC((Windows.Win32.Foundation.HWND)Handle);
+                using var g = Graphics.FromHdc(dc);
+                using var borderPen = new Pen(BorderColor);
+                g.DrawRectangle(borderPen, 0, 0, Width - 1, Height - 1);
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
         }
     }
 }
